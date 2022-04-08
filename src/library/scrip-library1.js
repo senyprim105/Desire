4a63c5d33af1a652f33495791535c8edb68789ef
senyprim105 <senyprim105@gmail.com>
2022-04-09 00:46:41 +1000

    mobile version

--- src/library/scrip-library1.js
+++
@@ -1,1 +0,0 @@
-export const libraryA=1;
--- src/pages/blog/blog.js
+++ src/pages/blog/blog.js
@@ -1,10 +1,11 @@
 require("../../components/layout/layout");
 $(function() {
-    $('.blog__slider-inner').slick({
+    $('.post__slider-inner').slick({
+        autoplay:true,
         autoplaySpeed: 3000,
         arrows: true,
-        prevArrow: `<button type="button" class="blog__slider-arrow blog__slider-arrow--prev"><img src="/img/icon-arow-left.svg"></button>`,
-        nextArrow: `<button type="button" class="blog__slider-arrow blog__slider-arrow--next"><img src="/img/icon-arow-right.svg"></button>`,
+        prevArrow: `<button type="button" class="post__slider-arrow post__slider-arrow--prev"><img src="/img/icon-arow-left.svg"></button>`,
+        nextArrow: `<button type="button" class="post__slider-arrow post__slider-arrow--next"><img src="/img/icon-arow-right.svg"></button>`,
         fade: true,
     });
 });
